13fb96da7e245c65bd210c047a9dee0ec019d983
xanqus <sbk98031816@gmail.com>
2022-08-22 02:16:15 +0900

    jwt server init

---
+++ src/main/java/com/example/jwtserver_forlecture/JwtServerForLectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.jwtserver_forlecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtServerForLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtServerForLectureApplication.class, args);
+	}
+
+}
