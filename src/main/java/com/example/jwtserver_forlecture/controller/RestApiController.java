397884420147e8aa5b3313e1f10ec53e3fefe6a7
xanqus <sbk98031816@gmail.com>
2022-08-22 22:56:25 +0900

    2강 jwtë£를 위한 yml 파일 세팅

---
+++ src/main/java/com/example/jwtserver_forlecture/controller/RestApiController.java
@@ -0,0 +1,15 @@
+package com.example.jwtserver_forlecture.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller("")
+public class RestApiController {
+
+    @GetMapping("/home")
+    @ResponseBody
+    public String home() {
+        return "home";
+    }
+}
